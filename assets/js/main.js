f5a69e7e51761e413db3022df602139012dbe0bd
Gustavo Prado <gustavo.prado47@outlook.com>
2025-09-29 15:03:38 -0300

    inicial commit
    menu mobile base pronto

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,31 @@
 $(document).ready(function () {
-    //abrir mobile
-  $("#abrirMenu").click(function () {
-    $(".intocavel").css("display", "block");
-    $(".sidebar").css("left", "0");
-  });
-  //fechar icone mobile
-  $("#fecharMenu").click(function () {
-    $(".intocavel").css("display", "none");
-    $(".sidebar").css("left", "-300px");
-  });
-  //fechar pelo fundo mobile
-  $(".intocavel").click(function (e) {
-    if (!$(e.target).closest(".sidebar").length) {
-      $(".intocavel").css("display", "none");
-      $(".sidebar").css("left", "-300px");
-    }
+  
+  const $sidebar = $(".sidebar-container");
+
+  // abrir
+  $("#menu").on("click", function (e) {
+    e.preventDefault();
+    $sidebar.addClass("active");
   });
 
+  // fechar
+  $(".sidebar-close").on("click", function (e) {
+    e.preventDefault();
+    $sidebar.removeClass("active");
+  });
+  //gsap animation
+  gsap.to("#img", {
+    // y: 30, // distância que sobe/ desce
+    // duration: 2, // duração da animação
+    // ease: "power2.inOut",
+    // repeat: -1, // repete infinitamente
+    // yoyo: true, // volta suavemente para posição inicial
 
-
+    scale: 1.1, // aumenta para 120%
+    duration: 0.6, // duração da animação em segundos
+    ease: "power1.inOut",
+    repeat: -1, // repete infinitamente
+    yoyo: true, // volta suavemente ao tamanho original
+  });
 });
+
